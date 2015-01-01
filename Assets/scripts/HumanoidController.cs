733d4eed42e1e6a3bf6ed7429c1c0121366937d1
puresamari <puresanari@gmail.com>
2015-01-01 19:19:26 +0100

    Rebuild HumanoidController

--- Assets/scripts/HumanoidController.cs
+++ Assets/scripts/HumanoidController.cs
@@ -5,17 +5,17 @@ using System;
 public class HumanoidController : MonoBehaviour {
     public float maxturn = 45;
 
-    public Quaternion headRotation = Quaternion.Euler(Vector3.zero);
-    public Quaternion spineRotation = Quaternion.Euler(Vector3.zero);
+	public GameObject head;
+	public GameObject spine;
 
     public void Look(Vector3 MoveMent) {
-        float newY = headRotation.eulerAngles.y + MoveMent.y;
+		float newY = head.transform.eulerAngles.y + MoveMent.y;
         if (newY > maxturn && newY < 360f - maxturn) {
-            newY = spineRotation.eulerAngles.y + MoveMent.y;
+			newY = spine.transform.eulerAngles.y + MoveMent.y;
             if (!(newY > maxturn && newY < 360f - maxturn))
-                spineRotation = Quaternion.Euler(headRotation.eulerAngles.x, newY, headRotation.eulerAngles.z);
+				spine.transform.rotation = Quaternion.Euler(head.transform.eulerAngles.x, newY, head.transform.eulerAngles.z);
         } else {
-            headRotation = Quaternion.Euler(headRotation.eulerAngles.x, newY, headRotation.eulerAngles.z);
+			head.transform.rotation = Quaternion.Euler(head.transform.eulerAngles.x, newY, head.transform.eulerAngles.z);
         }
     }
 }
