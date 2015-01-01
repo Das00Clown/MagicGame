81733c875c717ddb7bb8f1aaff265dd0e71d3d7c
puresamari <puresanari@gmail.com>
2015-01-01 19:07:18 +0100

    CameraEyeBinding

---
+++ Assets/scripts/CameraControll.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControll : MonoBehaviour {
+	public GameObject leftEye;
+	public GameObject rightEye;
+	void Update () {
+		transform.position = new Vector3 (Middle (leftEye.transform.position.x,rightEye.transform.position.x),leftEye.transform.position.y,leftEye.transform.position.z);
+		transform.rotation = new Quaternion(leftEye.transform.rotation.x,leftEye.transform.rotation.y,leftEye.transform.rotation.z,leftEye.transform.rotation.w);
+	}
+	float Middle(float f1, float f2) {
+		return f2 + ((f1 + f2 )/2);
+	}
+}
