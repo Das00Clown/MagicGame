9fd530498e62ba49c2cd2383e574b9a721514c85
puresamari <puresamari@gmail.com>
2015-01-02 11:38:54 +0100

    Major Update

--- Assets/scripts/CameraControll.cs
+++ Assets/scripts/CameraControll.cs
@@ -2,11 +2,16 @@
 using System.Collections;
 
 public class CameraControll : MonoBehaviour {
-	public GameObject leftEye;
-	public GameObject rightEye;
-	void Update () {
-		transform.position = new Vector3 (Middle (leftEye.transform.position.x,rightEye.transform.position.x),leftEye.transform.position.y,leftEye.transform.position.z);
-		transform.rotation = new Quaternion(leftEye.transform.rotation.x,leftEye.transform.rotation.y,leftEye.transform.rotation.z,leftEye.transform.rotation.w);
+	public GameObject player;
+    HumanoidController playerController;
+    void Start() {
+        playerController = player.GetComponent<HumanoidController>();
+    }
+    void Update() {
+        transform.rotation = Quaternion.Euler(playerController.headEuler.x, playerController.head.transform.eulerAngles.y, 0);
+        transform.position = new Vector3(player.transform.position.x, playerController.head.transform.position.y, player.transform.position.z);
+        //transform.position = new Vector3 (Middle (leftEye.transform.position.x,rightEye.transform.position.x),leftEye.transform.position.y,leftEye.transform.position.z);
+        //transform.rotation = new Quaternion(leftEye.transform.rotation.x,leftEye.transform.rotation.y,leftEye.transform.rotation.z,leftEye.transform.rotation.w);
 	}
 	float Middle(float f1, float f2) {
 		return f2 + ((f1 + f2 )/2);
--- Assets/scripts/HumanoidController.cs
+++ Assets/scripts/HumanoidController.cs
@@ -5,17 +5,28 @@ using System;
 public class HumanoidController : MonoBehaviour {
     public float maxturn = 45;
 
-	public GameObject head;
-	public GameObject spine;
+    public Vector3 headEuler = Vector3.zero;
+
+    public GameObject head;
+
+    public bool turn = false;
 
     public void Look(Vector3 MoveMent) {
-		float newY = head.transform.eulerAngles.y + MoveMent.y;
+		float newY = transform.eulerAngles.y + MoveMent.y;
         if (newY > maxturn && newY < 360f - maxturn) {
-			newY = spine.transform.eulerAngles.y + MoveMent.y;
-            if (!(newY > maxturn && newY < 360f - maxturn))
-				spine.transform.rotation = Quaternion.Euler(head.transform.eulerAngles.x, newY, head.transform.eulerAngles.z);
+
         } else {
-			head.transform.rotation = Quaternion.Euler(head.transform.eulerAngles.x, newY, head.transform.eulerAngles.z);
+            headEuler = new Vector3(transform.eulerAngles.x, newY, 0);
         }
+        turn = transform.eulerAngles.y == 0;
     }
 }
+
+        //float newY = head.transform.eulerAngles.y + MoveMent.y;
+        //if (newY > maxturn && newY < 360f - maxturn) {
+        //    newY = spine.transform.eulerAngles.y + MoveMent.y;
+        //    if (!(newY > maxturn && newY < 360f - maxturn))
+        //        spine.transform.rotation = Quaternion.Euler(spine.transform.eulerAngles.x, newY, spine.transform.eulerAngles.z);
+        //} else {
+        //    head.transform.rotation = Quaternion.Euler(head.transform.eulerAngles.x, newY, head.transform.eulerAngles.z);
+        //}
