df3eb1f168e619ac95afc686854b484edb17e000
puresamari <puresamari@gmail.com>
2015-01-02 22:21:40 +0100

    General animation overwrites - "AI"
    "AI" Integration beginns :dancer:

---
+++ Assets/NPC.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC : MonoBehaviour {
+
+    HumanoidController humanoidController;
+
+	void Start () {
+        humanoidController = GetComponent<HumanoidController>();
+	}
+
+
+    void Update() {
+        System.Random rnd = new System.Random();
+        humanoidController.Look(new Vector3(0, (float)(2 * rnd.NextDouble() -1), 0));
+        humanoidController.walk = System.Convert.ToBoolean( rnd.Next(0, 1));
+    }
+}
